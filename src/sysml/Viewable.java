d24c188da4a25c48254d968c22d5b3dcb6cb89e0
bclement <bclement@jpl.nasa.gov>
2014-04-22 08:00:34 -0700

    just adding a comment

--- src/sysml/Viewable.java
+++ src/sysml/Viewable.java
@@ -13,5 +13,8 @@ public interface Viewable< E > {
      * @return JSON for embedding in a SysML View (TODO -- include a specification)
      */
     public JSONObject toViewJson();
+    /**
+     * @return the elements whose information is accessible in a view
+     */
     public Collection<E> getDisplayedElements();
 }
